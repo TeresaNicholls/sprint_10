255fd24924e8364eb72941848bb09e1489fcb807
Teresa Nicholls <MrsNicholls2015@gmail.com>
2023-05-15 23:36:33 -0700

    adds a bigger query refactors python for connect to db into functions

--- module_1/queries.py
+++ module_1/queries.py
@@ -1,1 +1,53 @@
-SELECT_ALL = 'SELECT character_id, name FROM charactercreator_character;'
+SELECT_ALL = '''SELECT character_id, name
+                FROM charactercreator_character;'''
+SELECT_ALL_FIVE = '''SELECT name,hp
+                FROM charactercreator_character LIMIT 5;'''
+CHAR_ID_80PLUS = '''SELECT * FROM charactercreator_character
+                WHERE character_id>80;'''
+CHAR_ID_AND = '''SELECT character_id,name
+                FROM charactercreator_character
+                WHERE character_id>80
+                AND character_id<100;'''
+CHAR_ID_BETWEEN = '''SELECT character_id,name
+                FROM charactercreator_character
+                WHERE character_id BETWEEN 5 AND 12;'''
+UNIQUE_NAMES = '''SELECT DISTINCT name
+                FROM charactercreator_character;'''
+NAMES_GROUPBY = '''SELECT * FROM charactercreator_character
+                GROUP BY name;'''
+COUNT_GROUPBY = '''SELECT name, COUNT(*)
+                FROM charactercreator_character
+                GROUP BY name;'''
+COUNT_ORDER_BY_ASC = '''SELECT name, COUNT(*)
+                FROM charactercreator_character
+                GROUP BY name ORDER BY COUNT(*)ASC;'''
+COUNT_ORDER_BY_DESC = '''SELECT name, COUNT(*)
+                FROM charactercreator_character
+                GROUP BY name
+                ORDER BY COUNT(*)DESC;'''
+FILTER_WITH_HAVING = '''SELECT name, COUNT(*)
+                FROM charactercreator_character
+                GROUP BY name HAVING COUNT(*)=2;'''
+ANOTHER_WAY = '''SELECT name, COUNT(*)
+                FROM charactercreator_character
+                GROUP BY name HAVING COUNT(*)=2
+                ORDER BY name DESC;'''
+JOIN_TABLES = '''SELECT * FROM charactercreator_character
+                JOIN charactercreator_mage
+                ON charactercreator_character.character_id
+                =charactercreator_mage.character_ptr_id;'''
+RENAME_AS_ALIAS = '''SELECT * FROM charactercreator_mage AS ccm
+                JOIN charactercreator_character AS cc_char
+                ON cc_char.character_id=ccm.character_ptr_id;'''
+RENAME_COLUMN_ALIAS = '''SELECT name, COUNT(*) AS num_characters
+                FROM charactercreator_character GROUP BY name;'''
+# Instructor's example of how to properly tab over for such query statements.
+AVG_ITEM_WEIGHT_PER_CHARACTER = '''
+    SELECT cc_char.name, AVG(ai.weight) AS avg_item_weight
+    FROM charactercreator_character AS cc_char
+    JOIN charactercreator_character_inventory as cc_inv
+    ON cc_char.character_id=cc_inv.character_id
+    JOIN armory_item AS ai;
+    ON ai.item_id=cc_inv.item_id;
+    GROUP BY cc_char.character_id;
+    '''
--- module_1/sqlite_example.py
+++ module_1/sqlite_example.py
@@ -1,21 +1,25 @@
-# Step 0 - import sqlite3
 import sqlite3
 import queries as q
+import pandas as pd
 
-# Step 1 - connect to the database
-# triple-check the spelling of your database filename
-connection = sqlite3.connect('rpg_db.sqlite3')
 
-# Step 2 - make the 'cursor'
-cursor = connection.cursor()
+# DB Connect function
+def connect_to_db(db_name='rpg_db.sqlite3'):
+    return sqlite3.connect(db_name)
 
-# Step 3 - write a query
-# (See the queries.py file)
 
-# Step 4 - execute the query on the cursor and fetch the results
-# "pulling the results" from the cursor
-results = cursor.execute(q.SELECT_ALL).fetchall()
+def execute_q(conn, query):
+    # Make the "cursor"
+    curs = conn.cursor()
+    # Execute the query
+    curs.execute(query)
+    # Pull (and return) the results
+    return curs.fetchall()
 
-# Print out the first 5 items in results
+
 if __name__ == '__main__':
-    print(results[:5])
+    conn = connect_to_db()
+    results = execute_q(conn, q.AVG_ITEM_WEIGHT_PER_CHARACTER)
+    df = pd.DataFrame(results)
+    df.columns = ['name', 'average_item_weight']
+    df.to_csv('rpg_db.csv', index=False)
